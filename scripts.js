07905fccaa679d381f2acceaf82b35a39b8c57b5
Glitch (stop-asian-hate) <none>
2021-05-01 02:49:46 +0000

    ⛈🛣 Updated with Glitch

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 //uses jQuery to add drop shadow to cards on hover
 $(document).ready(function() {
- // executes when HTML-Document is loaded and DOM is ready
-console.log("document is ready");
-  $( ".card" ).hover(
-  function() {
-    $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
-  }, function() {
-    $(this).removeClass('shadow-lg');
-  }
-);
-  
-// document ready  
+  // executes when HTML-Document is loaded and DOM is ready
+  console.log("document is ready");
+  $(".card").hover(
+    function() {
+      $(this)
+        .addClass("shadow-lg")
+        .css("cursor", "pointer");
+    },
+    function() {
+      $(this).removeClass("shadow-lg");
+    }
+  );
+
+  // document ready
 });
